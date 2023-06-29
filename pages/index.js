cf2663341b16cc2c546d5940db1cfdac1a2af3a5
Abdulsamad sadiq <holarfemilekan011@gmail.com>
2023-06-29 14:45:10 +0100

    update UI

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Token Wordle</title>
+        <title>Words Search</title>
         <meta name="description" content="The tokenized wordle system" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
