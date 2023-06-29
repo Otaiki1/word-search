69ddf996413da2a03ec4d04cf2e2992203a946cd
Abdulsamad sadiq <holarfemilekan011@gmail.com>
2023-06-29 14:27:12 +0100

    refac: update UI

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar({ hideLogin }) {
           <img src="./images/Logo.png" alt="" className="w-24" />
         </div>
         <div
-          class="md:hidden block"
+          className="md:hidden block"
           id="burger"
           onClick={() => setShowMenu(!showMenu)}
         >
@@ -25,13 +25,13 @@ export default function Navbar({ hideLogin }) {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             class="w-10 h-12"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLineCap="round"
+              strokeLineJoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>
--- components/homepage/HomeCard.js
+++ components/homepage/HomeCard.js
@@ -1,12 +1,12 @@
-export default function HomeCard({amount, desc, key}) {
+export default function HomeCard({ amount, desc }) {
   return (
-    <div className="w-52 h-52 relative" key={key}>
+    <div className="w-52 h-52 relative">
       <img src="./images/curvyBg.png" alt="" className="w-full h-full" />
       <div className="absolute top-16 w-full">
-        <h1 className="text-white text-center text-5xl font-semibold">{amount}</h1>
-        <p className="text-white text-2xl mt-5 text-center">
-            {desc}
-        </p>
+        <h1 className="text-white text-center text-5xl font-semibold">
+          {amount}
+        </h1>
+        <p className="text-white text-2xl mt-5 text-center">{desc}</p>
       </div>
     </div>
   );
