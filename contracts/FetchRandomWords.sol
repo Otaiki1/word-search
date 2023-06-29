a2417022aacc7a8a6392c7f78113e74cc8216969
Abdulsamad sadiq <holarfemilekan011@gmail.com>
2023-06-30 00:32:38 +0100

    rfeat: add random word fetcher

--- contracts/FetchRandomWords.sol
+++ contracts/FetchRandomWords.sol
@@ -1,119 +1,70 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "./interfaces/IDIARandomOracle.sol";
 import "./EncryptionContract.sol";
 
-contract VRFD20 is VRFConsumerBaseV2, EncryptionContract {
-    uint256 private constant ROLL_IN_PROGRESS = 42;
-
-    VRFCoordinatorV2Interface COORDINATOR;
-
-    // Your subscription ID.
-    uint64 s_subscriptionId;
-
-    // see https://docs.chain.link/docs/vrf-contracts/#configurations
-    // address vrfCoordinator;
-
-    // The gas lane to use, which specifies the maximum gas price to bump to.
-    // For a list of available gas lanes on each network,
-    // see https://docs.chain.link/docs/vrf-contracts/#configurations
-    bytes32 s_keyHash;
-
-    // Depends on the number of requested values that you want sent to the
-    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
-    // so 40,000 is a safe default for this example contract. Test and adjust
-    // this limit based on the network that you select, the size of the request,
-    // and the processing of the callback request in the fulfillRandomWords()
-    // function.
-    uint32 callbackGasLimit = 40000;
-
-    // The default is 3, but you can set this higher.
-    uint16 requestConfirmations = 3;
-
-    // For this example, retrieve 1 random value in one request.
-    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
-    uint32 numWords = 1;
+contract DiceGame is EncryptionContract {
+    address public randomOracle;
 
     // map rollers to requestIds
-    mapping(uint256 => address) private s_rollers;
+    mapping(address => uint256) private s_rollers;
     // map vrf results to rollers
     mapping(address => uint256) private s_results;
     //map users address to request time
     mapping(address => uint256) private s_lastRequestTime;
 
     event DiceRolled(uint256 indexed requestId, address indexed roller);
-    event DiceLanded(uint256 indexed requestId, uint256 indexed result);
 
-    /**
-     * @notice Constructor inherits VRFConsumerBaseV2
-     *
-     * @dev NETWORK: specify upon deployment
-     *
-     * @param subscriptionId subscription id that this consumer contract can use
-     */
     constructor(
-        uint64 subscriptionId,
-        bytes32 _secretKey,
-        address vrfCoordinator,
-        bytes32 _keyHash
-    ) VRFConsumerBaseV2(vrfCoordinator) EncryptionContract(_secretKey) {
-        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-        s_subscriptionId = subscriptionId;
-        s_keyHash = _keyHash;
+        address oracle,
+        bytes32 _secretKey
+    ) EncryptionContract(_secretKey) {
+        randomOracle = oracle;
     }
 
-    /**
-     * @notice Requests randomness
-     * @dev Warning: if the VRF response is delayed, avoid calling requestRandomness repeatedly
-     * as that would give miners/VRF operators latitude about which VRF response arrives first.
-     * @dev You must review your implementation details with extreme care.
-     *
-     * @param roller address of the roller
-     */
-    function rollDice(address roller) public returns (uint256 requestId) {
+    function getRandomValue(
+        uint256 _round
+    ) public view returns (string memory) {
+        return IDIARandomOracle(randomOracle).getRandomValueFromRound(_round);
+    }
+
+    function fetchDailyWord() public {
         require(
-            block.timestamp >= s_lastRequestTime[roller] + 1 days,
+            block.timestamp >= s_lastRequestTime[msg.sender] + 1 days,
             "Can only request once per day"
         );
-        require(s_results[roller] == 0, "Already rolled");
-        // Will revert if subscription is not set and funded.
-        requestId = COORDINATOR.requestRandomWords(
-            s_keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
-        );
+        s_lastRequestTime[msg.sender] = block.timestamp;
 
-        s_rollers[requestId] = roller;
-        s_results[roller] = ROLL_IN_PROGRESS;
-        s_lastRequestTime[roller] = block.timestamp;
+        uint requestId = IDIARandomOracle(randomOracle).getLastRound();
+        s_rollers[msg.sender] = requestId;
 
-        emit DiceRolled(requestId, roller);
+        emit DiceRolled(requestId, msg.sender);
     }
 
-    /**
-     * @notice Callback function used by VRF Coordinator to return the random number to this contract.
-     *
-     * @dev Some action on the contract state should be taken here, like storing the result.
-     * @dev WARNING: take care to avoid having multiple VRF requests in flight if their order of arrival would result
-     * in contract states with different outcomes. Otherwise miners or the VRF operator would could take advantage
-     * by controlling the order.
-     * @dev The VRF Coordinator will only send this function verified responses, and the parent VRFConsumerBaseV2
-     * contract ensures that this method only receives randomness from the designated VRFCoordinator.
-     *
-     * @param requestId uint256
-     * @param randomWords  uint256[] The random result returned by the oracle.
-     */
-    function fulfillRandomWords(
-        uint256 requestId,
-        uint256[] memory randomWords
-    ) internal override {
-        uint256 d20Value = (randomWords[0] % 261) + 1;
-        s_results[s_rollers[requestId]] = d20Value;
-        emit DiceLanded(requestId, d20Value);
+    function rollDice() public {
+        // require(seed1 > 0, "Player 1 needs to commit their seed!");
+        // require(seed2 > 0, "Player 2 needs to commit their seed!");
+        require(
+            block.timestamp >= s_lastRequestTime[msg.sender] + 1 days,
+            "Can only request once per day"
+        );
+        uint latestRoundId = s_rollers[msg.sender];
+        uint _round = latestRoundId + 2;
+
+        require(
+            IDIARandomOracle(randomOracle).getLastRound() >= _round,
+            "Wait for the randmoness round to roll your dice."
+        );
+
+        string memory rand = getRandomValue(_round);
+        uint256 playerResult = (uint256(keccak256(abi.encodePacked(rand))) +
+            s_lastRequestTime[msg.sender]) % 261;
+
+        s_results[msg.sender] = playerResult;
+
+        emit DiceRolled(latestRoundId, msg.sender);
     }
 
     /**
@@ -121,10 +72,9 @@ contract VRFD20 is VRFConsumerBaseV2, EncryptionContract {
      * @param player address
      * @return word as a string
      */
-    function word(address player) public view returns (bytes32[5] memory) {
-        require(s_results[player] != 0, "Dice not rolled");
-        require(s_results[player] != ROLL_IN_PROGRESS, "Roll in progress");
-        string memory userWord = getWord(s_results[player]);
+    function word() public view returns (bytes32[5] memory) {
+        require(s_results[msg.sender] != 0, "Dice not rolled");
+        string memory userWord = getWord(s_results[msg.sender]);
 
         return encryptWord(userWord);
     }
@@ -398,6 +348,6 @@ contract VRFD20 is VRFConsumerBaseV2, EncryptionContract {
             "apart",
             "turns"
         ];
-        return words[id - 1];
+        return words[id];
     }
 }
